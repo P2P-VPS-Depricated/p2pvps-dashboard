737b21bd113b3f7ae7f41e0e47841390ce882eea
Chris Troutner <chris.troutner@gmail.com>
2018-03-25 14:43:06 -0700

    Working on production server

--- config/index.js
+++ config/index.js
@@ -8,6 +8,15 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    },
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
