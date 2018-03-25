7c68d3886883948c71da10f6e24b448a52054247
Chris Troutner <chris.troutner@gmail.com>
2018-03-25 14:53:25 -0700

    removing prod proxy table

--- config/index.js
+++ config/index.js
@@ -8,15 +8,6 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-      '/api': {
-        target: 'http://p2pvps.net:3001',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api'
-        }
-      }
-    },
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
